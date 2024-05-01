4e4897a3c4ba3345854dca31c5484bca8bd413a6
Sandra Prestel <38217124+SandraPrestel@users.noreply.github.com>
2024-05-01 14:58:53 +0200

    Test Timestamp

--- MMM-MedicationReminder.js
+++ MMM-MedicationReminder.js
@@ -4,11 +4,58 @@ Module.register("MMM-MedicationReminder", {
     defaults: {
         reminders: [{
             medicationname: "L-Thyroxin",
-            time: 2000,
+            time: 1430,
             duration: 60 * 60 * 1000
         }]
         
     },
+
+    currentList: [],
+
+    // when the mirror has started and all modules are loaded:
+    // check every minute, if a notification should be displayed and display the list of medications
+    start: function() {
+        var self = this;
+
+        // initial call
+        self.checkMedications();
+
+        setInterval(function() {
+            self.checkMedications();
+            self.displayMedications();
+        }, 60*1000);
+
+    },
+
+
+    checkMedications: function () {
+
+        let self = this;
+
+        this.config.reminders.array.forEach(function(medication, i) {
+            // get current time
+            currentDate = new Date();
+            currentTime = currentDate.toLocaleTimeString();
+            // check if the current time is within the timeframe of the reminder (time + duration)
+            console.log(currentTime);
+
+            // ... if yes: add to display list
+
+            // ... if not go to next
+        });
+
+        
+
+    },
+
+
+    // create the current list of notifications
+    displayMedications: function () {
+
+
+        self.updateDom();
+    },
+
 
     // Build the module display
     getDom: function () {
